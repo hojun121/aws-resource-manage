28d9eb13323e4602a8ca893fecf59ea3c3127451
jaehopark <107022839+hojun121@users.noreply.github.com>
2023-12-04 12:11:36 +0900

    Update aws-resource-mng-script.sh

--- aws-resource-mng-script.sh
+++ aws-resource-mng-script.sh
@@ -1,5 +1,5 @@
-export AWS_ACCESS_KEY_ID=AKIAVHB7HIAT7ERYSSFN
-export AWS_SECRET_ACCESS_KEY=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
+export AWS_ACCESS_KEY_ID=-----
+export AWS_SECRET_ACCESS_KEY=----
 export AWS_REGION=ap-northeast-2
 docker build -t steampipe-dashboard --build-arg AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --build-arg AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY --build-arg AWS_REGION=$AWS_REGION .
 docker run --name steampipe-dashboard -p9194:9194 -p9193:9193 -itd steampipe-dashboard
